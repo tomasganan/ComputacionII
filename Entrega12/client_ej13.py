b914b195597e13650790c865eb96642d5bddd872
Tomas Gañan <tomasganan@Tomas.local>
2021-08-17 11:34:17 -0300

    Avances en entrega 11 y 12

---
+++ Entrega12/client_ej13.py
@@ -0,0 +1,51 @@
+""" 
+Autor: GAÑÁN, Tomás 
+Entrega 12: Client (Ejercicio 13)
+"""
+# Importacion de librerias/modulos
+
+import getopt
+import socket
+import sys
+from datetime import datetime
+
+def main():
+    
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'a:p:l')
+    
+    for option, argument in opt:
+        if option == '-a':
+            address = argument
+        elif option == '-p':
+            port = int(argument)
+        elif option == 'l':
+            logFile = argument
+    
+    try:
+        # Crear socket TCP/IP
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Conexión del socket
+        print("Conexión establecida. Address: {}, Port: {}".format(address,port))
+        
+        s.connect((address, port))
+        
+        comando = False
+        
+        while comando != True:
+            comando = str(input('> '))
+            s.send(comando.encode())
+            answerServer = s.recv(1024).decode()
+            print(answerServer)
+
+            datetime = datetime.now().strftime("%d/%m/%Y %H:%M")
+            with open(logFile, 'a') as file:
+                file.write('[' + datetime + '] ||| ' + comando + '\n')
+        
+    finally:
+        # Cerrando conexión
+        print("Cerrando conexión")
+        s.close()
+
+if __name__ == "__main__":
+    main()
---
+++ Entrega12/server_ej13.py
@@ -0,0 +1,71 @@
+""" 
+Autor: GAÑÁN, Tomás 
+Entrega 12: Server (Ejercicio 13)
+"""
+# Importacion de librerias/modulos
+
+import getopt
+import socket
+import sys
+import multiprocessing
+import signal
+import os
+import subprocess as sp
+
+def closeServer(s, frame):
+    print("\n Cerrando conexiones...")
+    sys.exit(0)
+
+def gnuClient(clientSocket, clientAddress):
+    while True:
+        comando = clientSocket.recv(2048)
+
+        if comando.decode().startswith("cd"):
+            path = comando.decode().split(" ")[1]
+            try:
+                os.chdir(path)
+                clientSocket.send('Ok'.encode())
+            except FileNotFoundError:
+                clientSocket.send("El directorio no existe".encode())
+
+        if comando.decode() == 'exit':
+            clientSocket.send('Chau.'.encode())
+            break
+
+    print('Client', address, 'disconnected')
+    clientSocket.close()
+
+def main():
+    
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:')
+    
+    port = int(opt[0][1])
+
+    signal.signal(signal.SIGINT, closeServer)
+    localAddress = socket.gethostbyname(socket.getfqdn())
+
+    # Crear socket TCP/IP
+    socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Enlace de socket
+    socketServer.bind(('', port))
+    print("Servidor iniciando en la direccion: ", localAddress,"y puerto: ", port)
+        
+    # Escuchando conexiones entrantes
+    socketServer.listen(5)
+    
+    while True:
+        # Esperando conexión
+        print("Esperando conexiones...")
+        clientAddress, clientSocket = socketServer.accept()
+        
+        try:
+            print("Conexion desde: ", clientAddress)
+            
+            new_process = multiprocessing.Process(target=gnuClient, args=(clientSocket, clientAddress))
+
+            process = multiprocessing.Process()
+            process.start()
+                
+if __name__ == "__main__":
+    main()
